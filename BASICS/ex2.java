f2718011124c115bc4ecc8a5420404e4357c9c47
amrit16947 <amritjoshi2004@gmail.com>
2024-12-03 20:59:41 +0530

    3rd commit

---
+++ BASICS/ex2.java
@@ -0,0 +1,17 @@
+package BASICS;
+
+public class ex2 {
+
+    public static void main(String a[]){
+        int[] arr = {1, 2, 3, 4, 5,
+            6, 7, 8, 9, 10};
+            int n = arr.length;
+            int sum = 0;
+            for (int i = 0; i < n; i++) {
+                sum = sum + arr[i];
+                }
+                System.out.println("Sum of array elements: " + sum);
+
+    }
+    
+}
