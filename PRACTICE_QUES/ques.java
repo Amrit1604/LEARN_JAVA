1505f9087060151c168a13dab5dbced42324f53b
amrit16947 <amritjoshi2004@gmail.com>
2024-12-14 14:02:56 +0530

    Auto

--- PRACTICE_QUES/ques.java
+++ PRACTICE_QUES/ques.java
@@ -1,10 +1,9 @@
 package PRACTICE_QUES;
 import java.util.Scanner;
 
-// 1. Student Grades: In a classroom, you can use an array to store the grades of each student in a particular subject. Each element of the array would represent the grade of a specific student.
-
 public class ques {
     
+    // 1. Student Grades
     static void ques1() {
         int[] grades = {99, 89, 77, 92, 84};
         for (int i = 0; i < grades.length; i++) {
@@ -17,11 +16,8 @@ public class ques {
 
 
 
-    // 2. List of Phone Contacts: Your smartphone's contact list is essentially an array.
-    // Each entry in the list represents a contact, and you can easily search, add, or delete contacts using array-like data structures.
-
+    // 2. List of Phone Contacts with search
     static void ques2() {
-
         String[] contacts = {
             "Amrit - 950-1580-144", 
             "Pranjal - 778-6971-868",
@@ -31,49 +27,52 @@ public class ques {
 
         System.out.println("\nPhone Contacts List:");
         for (int i = 0; i < contacts.length; i++) {
-            System.out.println((i + 1) + ". " + contacts[i] + "\n");
+            System.out.println((i + 1) + ". " + contacts[i]);
         }
 
-        System.out.println("Enter the name to search");
+        System.out.println("Enter the name to search:");
         Scanner sc = new Scanner(System.in);
         String searchName = sc.nextLine().toLowerCase();
 
         boolean found = false;
-        for(int i=0;i<contacts.length; i++){
-            if(contacts[i].toLowerCase().contains(searchName)){
-                System.out.println("Found " + searchName + "'s contact " + "\n" + contacts[i]);
+        for (int i = 0; i < contacts.length; i++) {
+            if (contacts[i].toLowerCase().contains(searchName)) {
+                System.out.println("Found " + searchName + "'s contact:\n" + contacts[i]);
                 found = true;
                 break;
             }
         }
-        if(!found){
-            System.out.println(searchName + " Not found in the contact list ");
+        if (!found) {
+            System.out.println(searchName + " not found in the contact list.");
         }
         sc.close();
     }
 
 
 
-// 3. High Scores in a Game: Video games often use arrays to store and
-// display high scores achieved by players. Each element of the array represents a player's score.
 
 
 
+// 3. HighScore in a game
 
-
-
-
-    public static void main(String[] args) {
-        // ques1();
-        ques2();
+static void ques3(){
+    int[] highScores = { 100, 200, 300, 400, 500};
+    
+    System.out.println("Current High Score List");
+    for(int i = 0 ; i<highScores.length; i++){
+        System.out.println((i+1) + ". " + highScores[i]);
     }
+
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter your new score:");
+    int newScore = sc.nextInt();
+    sc.close();
+    
 }
 
-
-
-
-
-
-
-
-
+    public static void main(String[] args) {
+        // ques1();  // Student Grades
+        // ques2();  // Phone Contacts Search
+        ques3();    // High Scores in a Game
+    }
+}
