41d5fb5e574278752c53082de1f3a96f413170fa
amrit16947 <amritjoshi2004@gmail.com>
2024-12-14 16:37:13 +0530

    Auto

--- PRACTICE_QUES/ques.java
+++ PRACTICE_QUES/ques.java
@@ -67,7 +67,31 @@ static void ques3(){
     System.out.println("Enter your new score:");
     int newScore = sc.nextInt();
     sc.close();
-    
+
+    boolean scoreAdded = false;
+    for(int i = 0 ; i<highScores.length; i++){
+        if(newScore>highScores[i]){
+            for(int j = highScores.length-1; j>i; j--){
+                highScores[j] = highScores[j-1];
+            }
+            highScores[i] = newScore;
+            scoreAdded = true;
+            break;
+        }
+    }
+
+    if(scoreAdded){
+        System.out.println("New Score added");
+        for(int i =0 ; i< highScores.length;i++){
+            System.out.println((i+1) + ". " + highScores[i]);
+
+        }
+
+    }
+    else{
+        System.out.println("Score is too low to be added");
+    }
+
 }
 
     public static void main(String[] args) {
